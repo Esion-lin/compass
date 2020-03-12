7a4f8dd43e7fc86ea954e939658db9211bedb666
esion-lin <bigesion@gmail.com>
2020-03-12 17:12:22 +0800

    updata

--- docs/private_tangle/run_coordinator.sh
+++ docs/private_tangle/run_coordinator.sh
@@ -5,8 +5,6 @@ if [[ $# -gt 0 ]]; then
 fi
 for i in "${rep[@]}"; do
     echo "starting coordinator $i"
-    #valgrind --leak-check=full ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
-    #gdb -ex r -ex bt -ex q --args ./examples/hotstuff-app --conf hotstuff-sec${i}.conf > log${i} 2>&1 &
     ./docs/private_tangle/03_run_coordinator.sh -inception -Mulitiple -broadcast -hotstuff_port 1006${i} -hotstuff_recv_port 1008${i} > log${i} 2>&1 &
 done
 wait
